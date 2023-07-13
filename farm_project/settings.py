a4e55f9c7c69fda42cc07a1c76e253108b08915c
--add <temtopeayobami@gmail.com>
2023-07-13 13:45:29 +0100

    Pushed Admin

--- farm_project/settings.py
+++ farm_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,7 +124,22 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL  ='/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    'site_header': 'MyFarm Shop',
+    'site_brand': 'MyFarm Shop',
+    'site_logo': 'img/corny.webp',
+    'copyright': 'My-farm',
+}
